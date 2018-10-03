965f9b36373cdba7b0482a1d933b9fbe57845572
Unitedideas <unitedideas@gmail.com>
2018-10-03 12:43:30 -0700

    adding background image and container background colors, tuning padding for UI UX

--- events/urls.py
+++ events/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
--- lobos/settings.py
+++ lobos/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 try:
     from lobos.secrets import SECRET_KEY
 except ImportError:
@@ -48,7 +47,7 @@ ANYMAIL = {
     "MAILGUN_API_KEY": MAILGUN_ACCESS_KEY,
     "MAILGUN_SENDER_DOMAIN": 'mg.lobosevents.com',
 }
-EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"  # or sendgrid.EmailBackend, or...
+EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
 
 
 MIDDLEWARE = [
