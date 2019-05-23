1aeada94d3966189934f9a5326c845e10ce04b3f
unitedideas <unitedideas@gmail.com>
2019-05-23 11:57:55 -0700

    Added a verification email input (removing all copy paste methods) to the event registration form.
    ------ Create Confirmation Checking Page todo -------------
    
    todo: allow users to see if they are registered for a race via confirmation number, first and last name
    
    ------------- Event Registration Page todo ----------------
    
    todo: add more detail to the paypal cart
    
    todo: change the confirmation number to match the paypal confirmation number
    
    todo: change the confirmation email to non-html email
    
    todo: allow the discount of an entry fee, if more than one rider is registered and a discount code is used
    or todo: remove the paypal button and reveal the submit button if there is only one rider.
    
    -------------------- Merch Page todo ----------------------
    
    todo: Remove from max available on order from database (should do it on current state via add and remove functions)
    
    **currently using flat 8% for shipping
    
    todo: add a cost estimator API (Async) or
    
    todo: Store data in database then email purchaser
    
    todo: Redirect to a thanks you / confirmation page with confirmation number (using the paypal cart number)
    
    then
    
    notify someone to fulfill order

--- events/models.py
+++ events/models.py
@@ -227,53 +227,37 @@ class RiderProfile(models.Model):
         (MALE, 'Male'),
     )
     # user name displayed at login
-    event = models.ForeignKey(
-        Event, null=True, blank=True, on_delete=models.CASCADE)
-    user = models.ForeignKey(
-        User, null=True, blank=True, on_delete=models.CASCADE)
-    rider_number = models.CharField(
-        'OMRA Member Number', max_length=20, null=True, blank=True)
-    rider_cat = models.CharField(
-        'Rider Category', max_length=1000, null=True, blank=True)
-    rider_class = models.CharField(
-        'Rider Class (required)', max_length=1000, choices=RIDER_CLASS)
+    event = models.ForeignKey(Event, null=True, blank=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    rider_number = models.CharField('OMRA Member Number', max_length=20, null=True, blank=True)
+    rider_cat = models.CharField('Rider Category', max_length=1000, null=True, blank=True)
+    rider_class = models.CharField('Rider Class (required)', max_length=1000, choices=RIDER_CLASS)
     first_name = models.CharField('First Name (required)', max_length=300)
     last_name = models.CharField('Last Name (required)', max_length=300)
     email = models.EmailField('Email (required)', max_length=300)
     email2 = models.EmailField('Verify Email', max_length=300, null=True, blank=True)
-    gender = models.CharField(null=True, blank=True,
-                              max_length=10, choices=GENDER)
-    birth_date = models.DateField(
-        'Birth Date - Example: 12/14/1980 (required)')
-    phone_number = models.CharField(
-        'Phone Number', max_length=10, null=True, blank=True)
+    gender = models.CharField(null=True, blank=True, max_length=10, choices=GENDER)
+    birth_date = models.DateField('Birth Date - Example: 12/14/1980 (required)')
+    phone_number = models.CharField('Phone Number', max_length=10, null=True, blank=True)
     country = models.CharField(max_length=300, null=True, blank=True)
     address = models.CharField(max_length=300, null=True, blank=True)
     address_line_two = models.CharField(max_length=300, null=True, blank=True)
     city = models.CharField(max_length=300, null=True, blank=True)
-    state = models.CharField(max_length=2, null=True,
-                             blank=True, choices=STATES)
-    zip_code = models.CharField(
-        'Zip Code', max_length=5, null=True, blank=True)
-    emergency_contact_name = models.CharField(
-        'Emergency Contact Name', max_length=300, null=True, blank=True)
-    emergency_contact_phone = models.CharField(
-        'Emergency Contact Phone', max_length=10, null=True, blank=True)
-    bike_make = models.CharField(
-        'Bike Manufacturer (required)', max_length=20, choices=MAKES)
-    bike_displacement = models.CharField(
-        'Bike Displacement', max_length=10, null=True, blank=True)
+    state = models.CharField(max_length=2, null=True, blank=True, choices=STATES)
+    zip_code = models.CharField('Zip Code', max_length=5, null=True, blank=True)
+    emergency_contact_name = models.CharField('Emergency Contact Name', max_length=300, null=True, blank=True)
+    emergency_contact_phone = models.CharField('Emergency Contact Phone', max_length=10, null=True, blank=True)
+    bike_make = models.CharField('Bike Manufacturer (required)', max_length=20, choices=MAKES)
+    bike_displacement = models.CharField('Bike Displacement', max_length=10, null=True, blank=True)
     escort_name = models.CharField(
         'Escort’s Name: (Must register as Escort Rider) Required for riders under 16 on the day of the event',
         max_length=300, null=True, blank=True)
     group_name = models.CharField('Riding in a group? Enter their First and Last names here:', max_length=1000,
                                   null=True, blank=True)
-    merchandise_ordered = models.TextField(
-        max_length=1000, null=True, blank=True, default=None)
+    merchandise_ordered = models.TextField(max_length=1000, null=True, blank=True, default=None)
     registration_date_time = models.DateTimeField('Created Time', editable=True, null=True, blank=True,
                                                   auto_now_add=True)
-    confirmation_number = models.CharField(
-        max_length=30, null=True, blank=True)
+    confirmation_number = models.CharField(max_length=30, null=True, blank=True)
     discount_code = models.CharField(max_length=100, null=True, blank=True)
     start_time = models.TimeField(max_length=30, null=True, blank=True)
 
