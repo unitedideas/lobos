abf52bd9d852bc35238a1b41d56537d5697adec7
unitedideas <unitedideas@gmail.com>
2019-06-07 20:57:22 -0700

    working on adding more detail to paypal about the event registration transactions and items purchased
    ------------- Event Registration Page todo ----------------
    todo: add a feature to allow a discount code to be used and it removes that total amount from the total cost of the cart. Cost of the cart can not == $0.00
    
    todo: add more detail to the paypal cart
    
    todo: allow the discount of an entry fee, if more than one rider is registered and a discount code is used
    or todo: remove the paypal button and reveal the submit button if there is only one rider.

--- events/views.py
+++ events/views.py
@@ -866,7 +866,6 @@ def event_confirmation(request):
     return render(request, 'events/event_confirmation.html', args)
 
 
-
 def id_generator(size=8, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
